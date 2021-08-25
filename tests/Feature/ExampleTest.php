af60020c73ba7da7fac9141aca0b19764f8cf83d
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2021-08-25 12:18:57 -0300

    commit

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
+        $response = $this->get('/streets');
 
         $response->assertStatus(200);
     }
